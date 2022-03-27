6f335bafa5e46d4c2d2d6356ca0cb89118f2852c
unknown <zechariahadebayo42@gmail.com>
2022-03-28 00:08:22 +0100

    permissions and signals

--- Ebackend/apps.py
+++ Ebackend/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
 
 
+
 class EbackendConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Ebackend'
+    
+    def ready(self) -> None:
+        import Ebackend.signals
---
+++ Ebackend/signals.py
@@ -0,0 +1,11 @@
+from django.db.models.signals import post_save
+from .models import VendorProfile, BuyerProfile, Product
+from django.contrib.auth.models import Group, Permission, User
+
+# defining a callback for adding vendors to group
+def add_to_vendor_group(instance, created, **kwargs):
+    if created:
+        instance.vendor.groups.add(Group.objects.get(name='Vendors'))
+
+# sending the signal from the vendorprofile table
+post_save.connect(add_to_vendor_group, sender=VendorProfile, weak= False)
