ca7af0ef1b08d106a5179a47c65e3ac1f2129543
unknown <zechariahadebayo42@gmail.com>
2022-04-26 16:35:48 +0100

    separating total amount function from increment function in index.js

--- Ebackend/static/js/index.js
+++ Ebackend/static/js/index.js
@@ -77,34 +77,25 @@ cartCount()
 // get the total of products selected
 const totalAmount = ()=>{
     let productPrice = document.querySelectorAll('#p-price')
-    let quantity = document.querySelectorAll('#quantity-tag')
+    let quantity = document.querySelectorAll('#quantity-tag')//the quantity element
     let total= document.querySelector('.total')
     sum= 0
     for( let i= 0; i< productPrice.length; i++){
         sum += Number(productPrice[i].textContent.split('Price: $')[1])
-        let initialQuantity = Number(quantity[i].value) // keeping track of the initial quantity to check for increament of decrement
-        quantity[i].addEventListener('change', (e)=>{ //checking whether the quantity has changed, for each product
-            initialQuantity += Number(e.target.value)
-            console.log(initialQuantity)
-            if(Number(e.target.value) < initialQuantity){  //checking if I should deduct or add to the total amount
-                sum -= Number(productPrice[i].textContent.split('Price: $')[1])
-            }
-            else if(Number(e.target.value) > initialQuantity){
-                sum += Number(productPrice[i].textContent.split('Price: $')[1])
-            }
-        } )
     }
     total.textContent +=`$${sum}.0`
 }
 totalAmount()
 
 // increase or decrease the amount of product to be purchased
-// const increaseNdDecreaseAmount= ()=>{
-//     let quantity = document.querySelectorAll('#quantity-tag')
-//     let total= document.querySelector('.total')
-//      for (let j= 0; j< quantity.length; j++){
-//          console.log(quantity[j].value)
-//      }
-// }
+const increaseNdDecreaseAmount= ()=>{
+    let quantity = document.querySelectorAll('#quantity-tag')
+    let total= document.querySelector('.total')
+    for (let j= 0; j< quantity.length; j++){
+        quantity[j].addEventListener('change', (e)=>{
 
-// increaseNdDecreaseAmount()
+        })//event listener ends here
+    }
+}
+
+increaseNdDecreaseAmount()
