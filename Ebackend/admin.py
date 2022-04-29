199166fc79fd3afd405954e5c3f122c748c888f3
unknown <zechariahadebayo42@gmail.com>
2022-04-30 00:25:12 +0100

    correction

--- Ebackend/admin.py
+++ Ebackend/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from .models import VendorProfile, BuyerProfile, Product, Category, PurchasedProducts
 
 # registering my tables for view in the admin dashboard
-admin.site.register([VendorProfile, BuyerProfile,Product, Category],PurchasedProducts)
+admin.site.register([VendorProfile, BuyerProfile,Product, Category,PurchasedProducts])
